a587453a3fbf8a260a5c23808abd7f55a3c96433
jdw <jdw@bcferrycoder.com>
2015-01-05 19:24:25 +0000

    add code to push to stackato - timing out

--- push.rb
+++ push.rb
@@ -2,10 +2,6 @@ require 'net/http/post/multipart'
 
 auth = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjbG91ZF9jb250cm9sbGVyIiwiaWF0IjoxNDIwNDgyODc0LCJleHAiOjE0MjA1NjkyNzQsImNsaWVudF9pZCI6ImNmIiwic2NvcGUiOlsib3BlbmlkIiwicGFzc3dvcmQud3JpdGUiLCJjbG91ZF9jb250cm9sbGVyLmFkbWluIiwiY2xvdWRfY29udHJvbGxlci5yZWFkIiwiY2xvdWRfY29udHJvbGxlci53cml0ZSIsInNjaW0ucmVhZCIsInNjaW0ud3JpdGUiXSwianRpIjoiZDdjNmIwNTgtYTM2YS00ZjRlLTkzM2YtNmQxYmViNmQ1YzJlIiwidXNlcl9pZCI6IjFiMzYzMjNhLWYzMWEtNGMxMi1hYWRjLTA0Y2EzZDRkZGMwMCIsInN1YiI6IjFiMzYzMjNhLWYzMWEtNGMxMi1hYWRjLTA0Y2EzZDRkZGMwMCIsInVzZXJfbmFtZSI6ImpkdyIsImVtYWlsIjoiamR3QGpkdy5jb20ifQ.wznwqcrbmr0ecjxG9Pc1jcuO3QnoWzjInyPCQOCeJcY'
 
-#puts Net::HTTP.get('example.com', '/index.html') 
-
-#url = URI.parse('https://api.192.168.0.112.xip.io/info')
-
 ## /info, no auth
 uri = URI('https://api.192.168.0.112.xip.io/info')
 req = Net::HTTP::Get.new(uri.path)
@@ -30,10 +26,24 @@ res = Net::HTTP.start(
 end
 puts res.body
 
-##
-# File.upload("../application.zip") do |appzip|
-#   puts "upload"
-#   req = Net::HTTP::Post::Multipart.new url.path,
+## /app/GUID/bits
+uri = URI('https://api.192.168.0.112.xip.io/v2/apps/2713d496-9a6e-4513-b081-3b3a358853ee/bits')
+File.open("./application.zip") do |appzip|
+   puts "upload"
+   req = Net::HTTP::Put::Multipart.new uri.path,
+  "Authorization" => "#{auth}",
+  "name" => "node-chat",
+  "resources" => "[]",
+  "filename" => appzip
+  res = Net::HTTP.start(uri.host, uri.port,
+        :use_ssl => uri.scheme == 'https', 
+        :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+    https.request(req)
+  end
+end
+puts res.body
+
+exit
 
 ## THIS WORKS FOR UPLOAD SERVER
 puts "uploading to #{url}"
